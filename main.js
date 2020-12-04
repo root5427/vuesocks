10d87405081f17627dc50d828ff7382189f888bb
root5427 <shukrik27@gmail.com>
2020-12-04 15:04:36 +0500

    Third Lesson

--- main.js
+++ main.js
@@ -6,5 +6,8 @@ var app = new Vue({
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true,
     }
 })
